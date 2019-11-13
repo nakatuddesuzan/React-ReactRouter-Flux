9cc06e2915267f1ef1db35f23c7a05303955e41c
Suzan Nakatudde <suzannakatudde@Suzans-MacBook-Pro.local>
2019-11-13 11:40:49 -0600

    fix bug

--- flux-example/src/stores/courseStore.js
+++ flux-example/src/stores/courseStore.js
@@ -1,6 +1,6 @@
 import { EventEmitter } from "events";
 import dispatcher from "../appDispatcher";
-import actionTpyes from "../actions/actionTypes";
+import actionTypes from "../actions/actionTypes";
 
 const CHANGE_EVENT = "change";
 let _courses = [];
@@ -28,18 +28,18 @@ class CourseStore extends EventEmitter {
 const store = new CourseStore();
 
 dispatcher.register(action => {
-  switch (action.actionTpye) {
-    case actionTpyes.CREATE_COURSE:
+  switch (action.actionType) {
+    case actionTypes.CREATE_COURSE:
       _courses.push(action.course);
       store.emitChange();
       break;
-    case actionTpyes.UPDATE_COURSE:
+    case actionTypes.UPDATE_COURSE:
       _courses = _courses.map(course =>
         course.id === action.course.id ? action.course : course
       );
       store.emitChange();
       break;
-    case actionTpyes.LOAD_COURSES:
+    case actionTypes.LOAD_COURSES:
       _courses = action.courses;
       store.emitChange();
       break;
